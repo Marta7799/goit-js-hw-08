13c11f775a3a791cf4a761f8d644fc2265f58b97
Mart <marta.nikiel@yahoo.com>
2023-09-13 11:23:11 +0200

    added second task and installed vimeoPlayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+const keyStorage = 'videoplayer - current - time';
+
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem(keyStorage, e.seconds);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem(keyStorage))
+  .then(function (seconds) {
+    player.play().then(function () {
+      console.log('the videon was played');
+    });
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
